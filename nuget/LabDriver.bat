b6eef7b25b87ed3af9e792b85c611d925ae03527
VanilaMao <zmaohua@umich.edu>
2020-04-04 14:34:41 -0400

    remove nuget

--- nuget/LabDriver.bat
+++
@@ -1,7 +0,0 @@
-xcopy ..\Drivers\Prior LabDrivers\build /s /e /y
-xcopy ..\Drivers\Prime LabDrivers\build /s /e /y
-xcopy ..\bin\x64\Release\LabDrivers.dll LabDrivers\lib\net45 /y
-Nuget pack LabDrivers\LabDrivers.nuspec
-nuget sources Add -Name "GitHub" -Source "https://nuget.pkg.github.com/VanilaMao/index.json" -UserName VanilaMao -Password ed47406c703a8f342b728b3204d037b76f540689
-
-nuget push "LabDrivers.1.0.7.nupkg" -Source "GitHub"
--- nuget/LabSave.bat
+++
@@ -1,5 +0,0 @@
-xcopy ..\LabSave\bin\x64\Release\LabSave.dll LabSave\lib\net45 /y
-Nuget pack LabSave\LabSave.nuspec
-nuget sources Add -Name "GitHub" -Source "https://nuget.pkg.github.com/VanilaMao/index.json" -UserName VanilaMao -Password ed47406c703a8f342b728b3204d037b76f540689
-
-nuget push "LabSave.1.0.4.nupkg" -Source "GitHub"
